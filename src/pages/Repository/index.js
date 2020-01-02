7e7e0c520ccf3c5116b6bb975bdf11e620fb61cb
miroswd <altamirsantos93@hotmail.com>
2020-01-02 18:29:09 -0300

    Carregando API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository:{decodeURIComponent(match.params.rep)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {}, // Como é um único repositório, tem q ser iniciado como um objeto
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.rep);
+
+    // const repository = await api.get(`/repos/${repoName}`);
+    // const issues = await api.get(`/repos/${repoName}/issues`);
+
+    const [repository, issues] = await Promise.all([
+      // Executando as duas requisições ao mesmo tempo
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open', // Retornando 5 itens abertos
+          per_page: 5,
+        },
+      }), // Query params
+    ]); // Retornado dentro de um array
+    // console.log(repository);
+    // console.log(issue);
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <h1>Repository</h1>;
+  }
 }
